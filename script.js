987cef0f6d274f2e0cdc39745ef84d5534093de5
Rody Castellanos <rodycastellanosrodriguez@gmail.com>
2025-09-18 13:13:51 -0600

    Fondo gris agregado, h3 pintado de azul

--- script.js
+++ script.js
@@ -11,15 +11,4 @@ h5Elements.forEach((h5) => {
     h5.style.color = randomColor;
   }
 });
-const encabezado = document.getElementById('cambiaColor');
 
-encabezado.onclick = function colorAleatorio() {
-    const h3s= document.getElementsByTagName('h3')
-    const h1s= document.getElementsByTagName('h1')
-    const colors = ['red', 'green', 'blue'];
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    for (let i = 0; i < h3s.length; i++){
-        h3s[i].style.color = randomColor;
-        h1s[i].style.color = randomColor;
-    }
-}
